211f0029204dc2206c0663a3cafe27444f2cab7f
YSWFelicity <felicitywang0551@gmail.com>
2025-01-08 20:02:26 -0800

    Login Page

--- src/assets/wrappers/RegisterAndLoginPage.js
+++ src/assets/wrappers/RegisterAndLoginPage.js
@@ -4,25 +4,25 @@ const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
   align-items: center;
-  .logo {
-    display: block;
+  .logo{
+    display: blcok;
     margin: 0 auto;
     margin-bottom: 1.38rem;
   }
-  .form {
-    max-width: 400px;
-    border-top: 5px solid var(--primary-500);
+  .form{
+    max-width:400px;
+    border-top:5px solid var()(--primary-500);
   }
-  h4 {
+  h4{
     text-align: center;
     margin-bottom: 1.38rem;
   }
-  p {
+  p{
     margin-top: 1rem;
     text-align: center;
     line-height: 1.5;
   }
-  .btn {
+  .btn{
     margin-top: 1rem;
   }
   .member-btn {
